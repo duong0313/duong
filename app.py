90b797b6dc351b8ee0a34f71aadd860158f0d929
duong0313 <pigsmall031@gmail.com>
2025-05-04 01:58:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -50,4 +50,5 @@ def add():
     return render_template('add.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))  # lấy cổng từ biến môi trường Render
+    app.run(host='0.0.0.0', port=port)
